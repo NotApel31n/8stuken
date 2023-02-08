ce6af930796736902f2c692d64c4c397f5ad57ed
apel31n <isip_d.t.hatamov@mpt.ru>
2023-02-08 22:06:35 +0300

    Добавьте файлы проекта.

---
+++ 8 штука/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dd8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.MainMenu();
+        }
+    }
+}
---
+++ 8 штука/tb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace dd8
+{
+    internal class Table
+    {
+        static public void GetUsers()
+        {
+            List<User> users;
+            string table = File.ReadAllText("C:\\Users\\timba\\source\\repos\\8 штука\\tb.json");
+            users = JsonConvert.DeserializeObject<List<User>>(table);
+
+            Console.WriteLine("Таблица рекордов");
+            Console.WriteLine("Имя пользователя Кол-во/минута Кол-во/секунда");
+            foreach (User user in users)
+            {
+                Console.WriteLine($"{user.name} {user.SymbolsPerMinute} {user.SymbolsPerSecond}");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Чтобы вернуться в главное меню нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
